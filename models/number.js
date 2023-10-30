65be99d53339c09306999c2f7e70df181d629274
dliendo <dliendo@somoswoxi.com>
2023-10-30 17:18:52 -0300

    initial

---
+++ models/number.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+exports.schema = new mongoose.Schema({
+  number: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    required: true,
+    type: String,
+    enum: ['PENDING', 'SOLD', 'AVAILABLE'],
+    default: 'AVAILABLE'
+  },
+  soldTo: String,
+})
+
+exports.model = mongoose.connection.model('numbers', exports.schema)
